854423724e265f936130accf68c3d55a4413017a
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-07 11:29:59 -0400

    Set up userSchema and authentication packages

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // SETUP =========================================
 
 const express   = require("express"),
+app             = express(),
 methodOverride  = require("method-override"),
 bodyParser      = require("body-parser"),
 mongoose        = require("mongoose"),
 ejs             = require("ejs"),
-app             = express(),
+passport        = require("passport"),
+LocalStrategy   = require("passport-local"),
 Hero            = require("./models/hero"),
 Berry           = require("./models/berry"),
+User            = require("./models/user"),
 seedDB          = require("./seeds");
 
 
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/rest-practice", {useNewUrlParser: true, useFindAndModify: false});
 
 // Tells express to use ejs files in the views folder
 app.set('view engine', 'ejs');
@@ -24,9 +29,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Tells express to use methodOverride
 app.use(methodOverride("_method"));
 
-// Mongoose =========================================
-mongoose.connect("mongodb://localhost:27017/rest-practice", {useNewUrlParser: true, useFindAndModify: false});
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "One Two Buckle My Shoe Pikachu",
+  resave: false,
+  saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // UNCOMMENT THE SEEDDB LINE BELOW !!
 // TO REMOVE ALL HERO OBJECTS !!
