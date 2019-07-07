ae3c1ef4ec23815d657389626f7772e3250e87bc
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-07 12:29:25 -0400

    Add register and login routes. And other authentication relations.

--- app.js
+++ app.js
@@ -47,6 +47,14 @@ passport.deserializeUser(User.deserializeUser());
 // AND RESET SAMPLE FILES !!
 // seedDB();
 
+
+// declaring middleware function for all routes
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
 // ROUTES =========================================
 app.get("/", function(req, res){
   res.redirect("/heroes");
@@ -60,14 +68,14 @@ app.get("/heroes", function(req, res){
 });
 
 // NEW
-app.get("/heroes/new", function(req, res){
+app.get("/heroes/new", isLoggedIn, function(req, res){
   Berry.find({}, function(err, allBerries){
-    res.render("new", {berries: allBerries});
+    res.render("heroes/new", {berries: allBerries});
   });
 });
 
 // CREATE
-app.post("/heroes", function(req, res){
+app.post("/heroes", isLoggedIn, function(req, res){
 
   Hero.create(req.body.hero, function(err, newHero){
     if (err) {
@@ -80,7 +88,7 @@ app.post("/heroes", function(req, res){
 });
 
 // Create review
-app.post("/heroes/:id/review", function(req, res){
+app.post("/heroes/:id/review", isLoggedIn, function(req, res){
 
   Hero.findById(req.params.id, function(err, foundHero){
     if (err) {
@@ -104,20 +112,18 @@ app.post("/heroes/:id/review", function(req, res){
 // SHOW
 app.get("/heroes/:id", function(req, res){
 
-
-
   Hero.findById(req.params.id).populate("berries").exec(function(err, foundHero){
     if (err) {
       console.log("THERE WAS AN ERROR:", err);
       res.redirect("*");
     } else {
-      res.render("show", {hero: foundHero});
+      res.render("heroes/show", {hero: foundHero});
     }
   });
 });
 
 // EDIT
-app.get("/heroes/:id/edit", function(req, res){
+app.get("/heroes/:id/edit", isLoggedIn, function(req, res){
 
   Hero.findById(req.params.id, function(err, foundHero){
     if (err) {
@@ -125,14 +131,14 @@ app.get("/heroes/:id/edit", function(req, res){
       res.redirect("error");
     } else {
       Berry.find({}, function(err, allBerries){
-        res.render("edit", {hero: foundHero, berries: allBerries});
+        res.render("heroes/edit", {hero: foundHero, berries: allBerries});
       });
     }
   });
 });
 
 // UPDATE
-app.put("/heroes/:id", function(req, res){
+app.put("/heroes/:id", isLoggedIn, function(req, res){
 
   Hero.findByIdAndUpdate(req.params.id, req.body.hero, function(err, foundHero){
     if (err) {
@@ -145,7 +151,7 @@ app.put("/heroes/:id", function(req, res){
 });
 
 // DESTROY
-app.delete("/heroes/:id", function(req, res){
+app.delete("/heroes/:id", isLoggedIn, function(req, res){
 
   Hero.findByIdAndDelete(req.params.id, function(err){
     if (err) {
@@ -157,11 +163,66 @@ app.delete("/heroes/:id", function(req, res){
   });
 });
 
+// ========================================
+// AUTH ROUTES ============================
+// ========================================
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register")
+});
+
+// handle registration
+app.post("/register", function(req, res){
+  let newUser = new User({
+    username: req.body.username,
+  });
+
+  User.register(newUser, req.body.password, function(err, user){
+    if (err){
+      console.log(err);
+      return res.render("register");
+    }
+
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/heroes");
+    });
+  });
+});
+
+// show login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+// handle login
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/heroes",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+// logout route
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/heroes");
+});
 
 // ERROR
 app.get("*", function(req, res){
   res.render("error");
 });
+
+// middleware function
+function isLoggedIn(req, res, next){
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+
 
 
 
